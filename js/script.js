6aad42710857b578fbef775c4d9ec49e941bcb49
Alex <askona1995@gmail.com>
2020-08-25 13:15:50 +0300

    add function style

--- js/script.js
+++ js/script.js
@@ -1,4 +1,51 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели:?', '');
+let numberOfFilms,
+    numberOfLastFilm = 2;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели:?', '');
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели:?', '');
+    }
+}
+
+function detectPersonalLevel() {
+    if(personalMoviesDB.count < 10) {
+        alert('Watching little bit films');
+    } else if(personalMoviesDB.count >= 10 && personalMoviesDB.count <= 30){
+        alert('You are classic customer');
+    } else if(personalMoviesDB.count > 30) {
+        alert('You have superpower');
+    } else {
+        alert('error');
+    }  
+}
+
+function rememberMyFilms() {
+    for(let i = 0; i<numberOfLastFilm; i++) {
+        let fitstQuestion = prompt('Один из последних просмотренных фильмов:?'),
+         secondQuestion = prompt('На сколько оцените его:?');
+     
+         if(fitstQuestion != '' && secondQuestion != '' && fitstQuestion != null && secondQuestion != null && fitstQuestion.length < 50) {
+             personalMoviesDB.movies[fitstQuestion] = secondQuestion;
+             console.log('done');
+         } else {
+             console.log('error');
+             i--;
+         }
+     }
+}
+
+function showMyDB(hidden) {
+    if(!hidden) {
+        console.log(personalMoviesDB);
+    }
+}
+
+function writeYourGenres() {
+    for(let i = 0; i < 3; i++) {
+        personalMoviesDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}?`);
+    }
+}
 
 const personalMoviesDB = {
     count: numberOfFilms,
@@ -8,31 +55,16 @@ const personalMoviesDB = {
     privat: false,
 };
 
-let numberOfLastFilm = 2;
 
-for(let i = 0; i<numberOfLastFilm; i++) {
-   let fitstQuestion = prompt('Один из последних просмотренных фильмов:?'),
-    secondQuestion = prompt('На сколько оцените его:?');
+// start();
 
-    if(fitstQuestion != '' && secondQuestion != '' && fitstQuestion != null && secondQuestion != null && fitstQuestion.length < 50) {
-        personalMoviesDB.movies[fitstQuestion] = secondQuestion;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
+// rememberMyFilms();
 
- 
-}
+// detectPersonalLevel(); 
 
-if(personalMoviesDB.count < 10) {
-    alert('Watching little bit films');
-} else if(personalMoviesDB.count >= 10 && personalMoviesDB.count <= 30){
-    alert('You are classic customer');
-} else if(personalMoviesDB.count > 30) {
-    alert('You have superpower');
-} else {
-    alert('error');
-}
+writeYourGenres();
 
-console.log(personalMoviesDB);
+showMyDB(personalMoviesDB.privat);
+
+
+
